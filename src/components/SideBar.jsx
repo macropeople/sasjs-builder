5f0004f0878ffdd785de0b937e807124bc90493b
Krishna Acondy <kc.acondy@outlook.com>
2020-04-15 19:02:08 +0100

    feat(sidebar): add active state

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -2,9 +2,11 @@ import React, { useContext } from "react";
 import { Menu, Sidebar } from "semantic-ui-react";
 import SidebarItem from "./SidebarItem";
 import { AppContext } from "../context/appContext";
+import { useLocation } from "react-router-dom";
 
 const SideBar = () => {
   const { isNewJson } = useContext(AppContext);
+  const location = useLocation();
   return (
     <Sidebar
       as={Menu}
@@ -14,14 +16,25 @@ const SideBar = () => {
       vertical
       visible
     >
-      <SidebarItem icon="file" link="/import-export" text="Import / Export" />
+      <SidebarItem
+        icon="file"
+        link="/import-export"
+        text="Import / Export"
+        active={location.pathname === "/import-export"}
+      />
       <SidebarItem
         icon="settings"
         link="/configuration"
         text="Configuration"
         showLabel={isNewJson}
+        active={location.pathname === "/configuration"}
       />
-      <SidebarItem icon="server" link="/services" text="Services" />
+      <SidebarItem
+        icon="server"
+        link="/services"
+        text="Services"
+        active={location.pathname === "/services"}
+      />
     </Sidebar>
   );
 };
--- src/components/SidebarItem.jsx
+++ src/components/SidebarItem.jsx
@@ -3,10 +3,13 @@ import { Link } from "react-router-dom";
 import { Menu, Icon, Label } from "semantic-ui-react";
 import "./SidebarItem.scss";
 
-const SidebarItem = ({ icon, link, text, showLabel }) => {
+const SidebarItem = ({ icon, link, text, showLabel, active }) => {
   return (
     <Link to={link}>
-      <Menu.Item as="span" className="sidebar-item">
+      <Menu.Item
+        as="span"
+        className={active ? "sidebar-item active" : "sidebar-item"}
+      >
         <Icon name={icon} />
         {text}
         {showLabel && (
