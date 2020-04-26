d4763ac05c028151a32edef69c5031e25682d3c4
Krishna Acondy <kc.acondy@outlook.com>
2020-04-26 14:43:34 +0100

    fix(*): first attempt at duplicate column validation

--- src/components/HotTableDefinition.jsx
+++ src/components/HotTableDefinition.jsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useState, useEffect } from "react";
+import React, { useCallback, useState, useEffect, useRef } from "react";
 import { HotTable } from "@handsontable/react";
 import { isNonEmpty } from "../utils";
 
@@ -16,10 +16,10 @@ const mapColumns = (schema, data) => {
 
 const HotTableDefinition = ({ columns, onUpdate }) => {
   const [data, setData] = useState([]);
+  const tableRef = useRef();
   const columnNamevalidator = useCallback(
     (value, callback) => {
       const columnNames = columns.map((c) => c.title);
-      console.log("Validator call for value: ", value);
       callback(!columnNames.includes(value));
     },
     [columns]
@@ -31,20 +31,48 @@ const HotTableDefinition = ({ columns, onUpdate }) => {
   }, [columns]);
 
   const tableDefinitionSchema = [
-    { title: "title", type: "text", validator: columnNamevalidator },
-    { title: "type", type: "dropdown", source: ["numeric", "text"] },
+    {
+      title: "title",
+      type: "text",
+      validator: columnNamevalidator,
+      allowEmpty: false,
+    },
+    {
+      title: "type",
+      type: "dropdown",
+      source: ["numeric", "text"],
+      allowEmpty: false,
+    },
     { title: "label", type: "text" },
   ];
 
   return (
     <HotTable
       licenseKey="non-commercial-and-evaluation"
+      ref={tableRef}
       columns={tableDefinitionSchema}
       stretchH="none"
       rowHeaders={true}
       colWidths={395}
       minSpareRows={5}
       data={data}
+      afterValidate={(_, value, row, prop) => {
+        if (prop === 0) {
+          debugger;
+          const columnNames = columns.map((c) => c.title);
+          if (
+            columnNames.includes(value) &&
+            columnNames.indexOf(value) !== row
+          ) {
+            const cellMeta = tableRef.current.hotInstance.getCellMeta(
+              row,
+              prop
+            );
+            cellMeta.instance.setDataAtCell(row, prop, null);
+            tableRef.current.hotInstance.render();
+          }
+        }
+      }}
       afterChange={(e) => {
         if (!!e) {
           const mappedColumns = mapColumns(
